7b0d294a67f2c24648ac0df888fb50c8387dde1c
Markus Skyttner <mskyttner@pm.me>
2023-10-12 19:10:55 +0200

    remove scripts that use uplot since it was removed from the container image

--- test_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --name duckrg -d -p "80:12321" duckrg duckrg --db /data/tpch.db
-sleep 3
-curl -sX POST -d @select.json -H "Content-Type: application/json" localhost/tpch | json_pp
-docker stop duckrg && docker rm -f duckrg
-
-docker run --rm -v $(pwd)/gh_youplot.sh:/tmp/gh_youplot.sh duckrg /tmp/gh_youplot.sh
